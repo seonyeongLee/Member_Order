3637fed99a67e8cdc7036fe49295d227b6bb8ab1
seonyeongLee <qrsty9636@gmail.com>
2021-06-20 21:58:41 +0900

    edit source code

--- src/main/java/com/api/idus/common/utility/ErrorUtility.java
+++ src/main/java/com/api/idus/common/utility/ErrorUtility.java
@@ -3,7 +3,7 @@ package com.api.idus.common.utility;
 public abstract class ErrorUtility {
 
     public enum EXCEPTION_TYPE {
-        DATA_NOT_FOUND, DATA_BINDING, USER_NOT_FOUND, USER_DATA_CONFLICT, VIEW_RENDERING, RECAPTCHA_API
+        DATA_NOT_FOUND, USER_DATA_CONFLICT
     }
 
 }
--- src/main/java/com/api/idus/common/utility/SessionUtility.java
+++
@@ -1,6 +0,0 @@
-package com.api.idus.common.utility;
-
-public class SessionUtility {
-
-
-}
